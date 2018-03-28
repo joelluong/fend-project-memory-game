6f279f0d78cfe399ddbb74061ac91ef2c5d48e70
DuongLuong <luongdaitrungduong@gmail.com>
2018-03-28 23:17:33 +1100

    Adding function to open cards

--- js/app.js
+++ js/app.js
@@ -63,3 +63,8 @@ document.addEventListener('DOMContentLoaded', function() {
  *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
+ function openCard (card)
+ {
+         card.classList.add(openClassName);
+         card.classList.add(showClassName);
+ };
